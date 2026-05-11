cf6624e34b53b260309b817112c7f0912f92d8a6
Cristea Bianca <bianca.cristea01@yahoo.com>
2026-05-11 15:34:31 +0300

    Add global exception handling closes #9

--- backend/src/main/java/org/example/backend/payload/APIResponse.java
+++ backend/src/main/java/org/example/backend/payload/APIResponse.java
@@ -2,14 +2,11 @@ package org.example.backend.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class APIResponse {
 
     private String message;
-    private String status;
-
+    private boolean status;
 }
