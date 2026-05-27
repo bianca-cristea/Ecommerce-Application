fa182acaae7554decfa0a3d291e2b8c264a08953
Cristea Bianca <bianca.cristea01@yahoo.com>
2026-05-27 20:26:03 +0300

    setup frontend closes #16

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { FaBeer } from 'react-icons/fa';
+import './App.css'
+ 
+
+function App() {
+ return (  
+  <h1 class="text-3xl font-bold underline text-black">
+   Welcome <FaBeer />
+  </h1>  )
+
+}
+
+export default App
